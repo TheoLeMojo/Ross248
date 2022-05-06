aa8860624472fdfcb5547a578724e35b2b19611c
wangyiiing <82363734+wangyiiing@users.noreply.github.com>
2022-05-06 12:16:52 +0200

    Add files via upload

--- Ross248/htdocs/faq.php
+++ Ross248/htdocs/faq.php
@@ -39,30 +39,54 @@
     <h1>Consultez la Foire aux questions !</h1>
     <div class="container-faq">
 
-    <div class="questions">
-        <div class="visible-pannel">
-            <h2>Paiement </h2>
-            <img src="ressources/croix.svg" alt="croix animation">
+      <div class="accordion">
+        <div class="accordion-item">
+          <button id="accordion-button-1" aria-expanded="false">
+            <span class="accordion-title">Quels sont les moyens de paiement accepté?</span>
+            <span class="icon" aria-hidden="true"></span>
+          </button>
+          <div class="accordion-content">
+            <p>
+            Vous pouvez régler par cartes bancaires,PayPal.
+            </p>
+          </div>
         </div>
-        <div class="toggle-pannel">
-            <h4>Quels sont les moyens de paiement accepté? </h4>
-            <p>Vous pouvez régler par cartes bancaires,PayPal.</p>
+        <div class="accordion-item">
+          <button id="accordion-button-2" aria-expanded="false">
+            <span class="accordion-title">Je n'ai pas reçu d'e-mail avec mes résultats de test.Que devrais-je faire?</span>
+            <span class="icon" aria-hidden="true"></span>
+          </button>
+          <div class="accordion-content">
+            <p>
+            Tout d'abord, veuillez vérifier votre dossier Spam - il est possible que votre fournisseur de messagerie ait mis notre message là-bas
+            </p>
+          </div>
         </div>
-    </div>
+        <div class="accordion-item">
+          <button id="accordion-button-3" aria-expanded="false">
+            <span class="accordion-title">Comment s'inscrire sur le site? </span>
+            <span class="icon" aria-hidden="true"></span>
+          </button>
+          <div class="accordion-content">
+            <p>
+            </p>
+          </div>
+        </div>
+        <div class="accordion-item">
+          <button id="accordion-button-4" aria-expanded="false">
+            <span class="accordion-title">Comment supprimer mon compte?</span>
+            <span class="icon" aria-hidden="true"></span>
+          </button>
+          <div class="accordion-content">
+            <p>
 
-    <div class="questions">
-        <div class="visible-pannel">
-            <h2>les résultat de test</h2>
-            <img src="ressources/croix.svg" alt="croix animation">
+            </p>
+          </div>
         </div>
-        <div class="toggle-pannel">
-            <h4>Je n'ai pas reçu d'e-mail avec mes résultats de test.Que devrais-je faire?</h4>
-            <p>Tout d'abord, veuillez vérifier votre dossier Spam - il est possible que votre fournisseur de messagerie ait mis notre message là-bas</p>
-        </div>
+        
+      </div>
     </div>
-
-    </div>
-</div>
+    
 
 
 
