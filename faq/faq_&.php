dd2e2dbbc3cfab139e9bb68ac8ef680f06bd220c
wangyiiing <82363734+wangyiiing@users.noreply.github.com>
2022-05-30 23:14:51 +0200

    Add files via upload

---
+++ faq/faq_&.php
@@ -0,0 +1,93 @@
+<!doctype html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="Cache-control" content="private" />
+  <title>Ross248</title>
+  <meta name="description" content="Notre solution a pour but de garantir la fiabilité des pilotes, au travers de divers tests psychotechniques.">
+  <meta name="robots" content="index, follow" />
+  <link rel="icon" type="image/png" href="../img/logo.png">
+  <link rel="stylesheet" href="../css/nav.css">
+  <link rel="stylesheet" href="../css/index2.css">
+  <link rel="stylesheet" href="../css/backtotop.css">
+  <link rel="stylesheet" href="../css/footer.css">
+  <link href="https://fonts.googleapis.com/css?family=Oswald:200,300,400,500,600,700&display=swap" rel="stylesheet">
+  <?php include('link.php')?> 
+</head>
+<body>
+<!-- Nav -->
+<?php include('nav.php')?>
+
+  <div id="haut">
+    <div class=videNav></div>
+    <div class="imgtop">
+      <!--<img src="https://i.imgur.com/wxYQfml.jpg"></img>-->
+      <div class="imgTexte">
+        <h1>FAQ</h1>
+        <h4></h4>
+        <div class="boutonBas">
+          <a href="#milieuImg">
+            <ion-icon name="arrow-dropdown-circle"></ion-icon>
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+
+<div class="containen-fluid">
+      <h2>Consultez la Foire aux questions !</h2>
+
+      <div class="accordion">
+         <div class="icon"></div>
+         <h5>Quels sont les moyens de paiement accepté?</h5>
+      </div>
+      <div class="panel">
+         <p>
+         Vous pouvez régler par cartes bancaires,PayPal. 
+         </p>
+      </div>
+       
+      <div class="accordion">
+         <div class="icon"></div>
+         <h5>Quels sont les moyens de paiement accepté?</h5>
+      </div>
+      <div class="panel">
+         <p>
+         Vous pouvez régler par cartes bancaires,PayPal. 
+         </p>
+      </div>
+            
+      <div class="accordion">
+         <div class="icon"></div>
+         <h5>Quels sont les moyens de paiement accepté?</h5>
+      </div>
+      <div class="panel">
+         <p>
+         Vous pouvez régler par cartes bancaires,PayPal. 
+         </p>
+      </div>
+            
+   </div>
+    
+
+
+
+  <a id="backtotop" class = "btntop" role="button" href="#" accesskey="t">
+  <i>
+    <img src = "../img/arrowtop.png" width="40" height="40">
+  </i>
+</a>
+
+<!-- Footer -->
+<?php include('footer.php') ?>
+
+  <!--<script src="js/script.js"></script>-->
+  <script src="../js/faq.js"></script>
+  <script src="../js/app.js"></script>
+  <script src="https://unpkg.com/ionicons@4.5.10-0/dist/ionicons.js"></script>
+  <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+  <script src="../js/index.js"></script>
+  <script src="../js/base.js"></script>
+</body>
+</html>
